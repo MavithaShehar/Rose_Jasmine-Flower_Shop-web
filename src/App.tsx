28e1cae729399299d718e36c17c2c2c74bc11942
Shehar Mavitha <mavithashehar@gmail.com>
2025-03-14 19:37:54 +0530

    update admin and user routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,40 @@ import ContactSection from './Sections/contactSection';
 import { Route, Routes } from 'react-router';
 import Home from './Sections/Home';
 import Admin from './Sections/Admin';
-//import AdminNave from './Sections/AdminNave';
+import AdminNave from './Components/Navebar/AdminNave';
+import AdminMakeDashBordPost from './Sections/AdminMakeDashBordPost';
 
 function App() {
   return (
-    <main className="bg-amber-950 h-fit w-full ">
+    <main className='w-full  bg-black'>
 
-
+    <div>
       {/* Home */}
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<ContactSection />} />
-      </Routes>
+      {/* <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<ContactSection />} />
+    </Routes> */}
+    </div>
 
-  
-    </main>
+    {/* Admin  */}
+    <div className='w-full h-full bg-blue-700 flex'>
+
+
+      <div className='w-1/5 h-screen bg-red-700'>
+        <AdminNave />
+      </div>
+
+      <div className='w-4/5 h-screen bg-green-400'>
+        <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/dashbord" element={<AdminMakeDashBordPost/>} />
+      </Routes>
+      </div>
+    </div>
+
+
+  </main>
   );
 }
 
