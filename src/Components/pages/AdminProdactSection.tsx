c6c77672074dc703b5aef36c4fda32f440095f32
Shehar Mavitha <mavithashehar@gmail.com>
2025-03-16 03:07:41 +0530

    create AdminProdactSection create prodact card butn

--- src/Components/pages/AdminProdactSection.tsx
+++ src/Components/pages/AdminProdactSection.tsx
@@ -2,8 +2,13 @@ function AdminMakeDashBordPost() {
     return (
         <section className=" relative w-full h-fit bg-blue-700 flex flex-col">
            
-            <div className="sticky top-0 right-0 z-10 flex items-center justify-end w-full h-20  border-2 border-solid">
-                <h1 className="text-5xl text-red-800 font-bold pl-20">Admin</h1>
+         <div className="sticky top-0 right-0 z-10 bg-white flex items-center justify-end w-full h-20  border-2 border-solid">
+        <button
+          
+          className="my-2 mr-10 p-2 bg-green-800 text-white font-semibold rounded-lg hover:bg-green-600 transition duration-300 active:bg-green-800"
+        >
+          Create Poster
+        </button>
             </div>
 
             <div className="w-full overflow-y-auto  pt-2">
