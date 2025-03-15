e889c123befc88ca41a238050d72843534ff7ea1
Shehar Mavitha <mavithashehar@gmail.com>
2025-03-16 03:13:01 +0530

    made popup screen when click createProdat button

--- src/Components/pages/AdminProdactSection.tsx
+++ src/Components/pages/AdminProdactSection.tsx
@@ -1,14 +1,25 @@
-function AdminMakeDashBordPost() {
+import { useState } from "react";
+import PopupProdact from "../common/popUp/PopupProdact";
+
+function AdminProdactSection() {
+
+     const [visible, setVisible] = useState(false);
+    
+      const closeModal = () => {
+        setVisible(false);
+      };
+      
     return (
         <section className=" relative w-full h-fit bg-blue-700 flex flex-col">
            
          <div className="sticky top-0 right-0 z-10 bg-white flex items-center justify-end w-full h-20  border-2 border-solid">
         <button
-          
+          onClick={() => setVisible(true)}
           className="my-2 mr-10 p-2 bg-green-800 text-white font-semibold rounded-lg hover:bg-green-600 transition duration-300 active:bg-green-800"
         >
-          Create Poster
+          Create Prosact
         </button>
+        {<PopupProdact isOpen={visible} onRequestClose={closeModal} />}
             </div>
 
             <div className="w-full overflow-y-auto  pt-2">
@@ -42,4 +53,4 @@ function AdminMakeDashBordPost() {
     );
 }
 
-export default AdminMakeDashBordPost;
+export default AdminProdactSection;
