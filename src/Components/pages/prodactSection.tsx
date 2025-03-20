269139f2da00fa1ca54c758cd0594ca62b7de3cb
Shehar Mavitha <mavithashehar@gmail.com>
2025-03-20 21:19:29 +0530

    made add card fill and loding on user prodact section

--- src/Components/pages/prodactSection.tsx
+++ src/Components/pages/prodactSection.tsx
@@ -1,35 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import jsonData from "../../../public/data/prodact.json"; 
 
 interface Flower {
-  id: number;
+  _id: string;
   name: string;
   color: string;
   price: number;
   status: string;
-  image: string;
+  imageurl: string;
   category: string;
 }
 
-const ProductSection: React.FC = () => {
-  const flowers: Flower[] = jsonData.flowers;
+function ProductSection(){
 
+  const [flowers, setFlowers] = useState<Flower[]>([]);
+
+  useEffect(() => {
+    const fetchFlowers = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/products");
+        const data = await response.json();
+        
+        if (response.ok) {
+          console.log(data)
+          setFlowers(data); // Assuming `data` is an array
+        } else {
+          alert(data.message || "Something went wrong.");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        alert("Something went wrong.");
+      }
+    };
+    fetchFlowers();
+  }, []);
+      
   return (
     <section className="bg-red-800 py-10">
       <div className="w-full pl-20 pr-20 bg-amber-300">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 bg-amber-900">
           {flowers.map((flower) => {
-            if (flower.status === "Active") {
+            if (flower.status === "active") {
                 return (
                     <div
-                      key={flower.id}
+                      key={flower._id}
                       className="bg-white p-1 rounded-lg shadow-lg transform transition-all hover:scale-105 hover:shadow-2xl"
                     >
                       <div className="flex flex-col items-center bg-amber-300">
                         {/* Background image */}
                         <div
                           style={{
-                            backgroundImage: `url(${flower.image})`,
+                            backgroundImage: `url(${flower.imageurl})`,
                           }}
                           className="w-full h-64 rounded-1xl bg-center bg-cover mb-4"
                         ></div>
